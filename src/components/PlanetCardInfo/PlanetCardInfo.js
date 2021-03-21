93fb9faaae80f8957559d418cb05f878631830d1
Oscar Barbosa <oscarfbarbosa@gmail.com>
2021-03-20 22:42:24 -0400

    Added API endpoints for calculations

--- src/components/PlanetCardInfo/PlanetCardInfo.js
+++ src/components/PlanetCardInfo/PlanetCardInfo.js
@@ -1,4 +1,4 @@
-import React , {Component} from 'react';
+import React from 'react';
 import classes from './PlanetCardInfo.module.css';
 
 const PlanetCardInfo = (props) => (
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React , {Component} from 'react';
 import PlanetCardList from '../components/PlanetCardList/PlanetCardList';
 import classes from './Home.module.css';
+import CalculationsService from '../api/CalculationsService.js';
 
 const title = "Real Time Astronomy";
 const aboutText = "An astronomy engine to calculate heliocentric and geocentric distances, and additional orbital data of planets from the solar system in real time. This is an upgraded version."
@@ -30,7 +31,36 @@ class Home extends Component{
         })
     }
 
+    retrieveMercuryCalculations(){
+
+      CalculationsService.retrieveMercuryCalculations()
+      .then(response => this.handleSuccessfulResponse(response))
+      .catch(error => this.handleError(error))
+    }
+
+    handleSuccessfulResponse(response){
+        console.log("Response: " + response.data[0][0]);
+    }
+
+    handleError(error){
+        console.log(error);
+        
+        let errorMessage = '';
+        
+        if(error.message){
+            errorMessage += error.message
+        }
+
+        if(error.message && error.response.data){
+            errorMessage += error.response.data.message;
+        }
+
+        this.setState({seconds : errorMessage});
+    }
+
     tick() {
+
+        this.retrieveMercuryCalculations();
         this.setState(state => ({
             seconds: state.seconds + 1
         }));
